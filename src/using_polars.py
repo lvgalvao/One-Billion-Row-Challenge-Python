c3b514e4e4e7b1f6cdab8eb9f66e8bf1893511ea
Luciano Filho <lvgalvaofilho@gmail.com>
2024-02-14 17:16:52 -0300

    feat: adicionar schema

--- src/using_polars.py
+++ src/using_polars.py
@@ -8,7 +8,7 @@ def create_polars_df():
     pl.Config.set_streaming_chunk_size(4000000)
     return (
         
-        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"])
+        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
         .group_by(by="station")
         .agg(
             max = pl.col("measure").max(),
