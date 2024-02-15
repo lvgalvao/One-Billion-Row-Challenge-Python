b24f9e22bf128faebcb134466f9b2cab7c3a3853
Luciano Filho <lvgalvaofilho@gmail.com>
2024-02-15 06:19:24 -0300

    feat: adicionar tqdm para barra de progresso em Python

--- src/using_python.py
+++ src/using_python.py
@@ -1,16 +1,21 @@
 from csv import reader
 from collections import defaultdict, Counter
+from tqdm import tqdm  # barra de progresso
 import time
 
+NUMERO_DE_LINHAS = 1_000_000_000
+
 def processar_temperaturas(path_do_csv):
-    minimas = defaultdict(lambda: 99.9)
-    maximas = defaultdict(lambda: -99.9)
+    # utilizando infinito positivo e negativo para comparar
+    minimas = defaultdict(lambda: float('inf'))
+    maximas = defaultdict(lambda: float('-inf'))
     somas = defaultdict(float)
     medicoes = Counter()
 
     with open(path_do_csv, 'r') as file:
         _reader = reader(file, delimiter=';')
-        for row in _reader:
+        # usando tqdm diretamente no iterador, isso mostrará a porcentagem de conclusão.
+        for row in tqdm(_reader, total=NUMERO_DE_LINHAS, desc="Processando"):
             nome_da_station, temperatura = str(row[0]), float(row[1])
             medicoes.update([nome_da_station])
             minimas[nome_da_station] = min(minimas[nome_da_station], temperatura)
@@ -19,19 +24,17 @@ def processar_temperaturas(path_do_csv):
 
     print("Dados carregados. Calculando estatísticas...")
 
-    # Dicionário para armazenar os resultados calculados
+    # calculando min, média e max para cada estação
     results = {}
-
-    # Calculando min, média e max para cada estação
     for station, qtd_medicoes in medicoes.items():
-        mean_temp = somas[station]/qtd_medicoes
+        mean_temp = somas[station] / qtd_medicoes
         results[station] = (minimas[station], mean_temp, maximas[station])
 
     print("Estatística calculada. Ordenando...")
-    # Ordenando os resultados pelo nome da estação
+    # ordenando os resultados pelo nome da estação
     sorted_results = dict(sorted(results.items()))
 
-    # Formatando os resultados para exibição
+    # formatando os resultados para exibição
     formatted_results = {station: f"{min_temp:.1f}/{mean_temp:.1f}/{max_temp:.1f}"
                          for station, (min_temp, mean_temp, max_temp) in sorted_results.items()}
 
@@ -39,7 +42,7 @@ def processar_temperaturas(path_do_csv):
 
 
 if __name__ == "__main__":
-    path_do_csv = "data/measurements.txt"
+    path_do_csv = "data/measurements1M.txt"
 
     print("Iniciando o processamento do arquivo.")
     start_time = time.time()  # Tempo de início
