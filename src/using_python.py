c06a1d4b9a608e6d91e40d1cba1ab9321ed64818
EuMateus551 <ymatheus551@gmail.com>
2025-09-18 17:20:42 -0300

    fix: ajustar leitura do CSV no desafio

--- src/using_python.py
+++ src/using_python.py
@@ -12,7 +12,7 @@ def processar_temperaturas(path_do_csv):
     somas = defaultdict(float)
     medicoes = Counter()
 
-    with open(path_do_csv, 'r') as file:
+    with open(path_do_csv, 'r', encoding='utf-8') as file:
         _reader = reader(file, delimiter=';')
         # usando tqdm diretamente no iterador, isso mostrará a porcentagem de conclusão.
         for row in tqdm(_reader, total=NUMERO_DE_LINHAS, desc="Processando"):
